49baff077051b42882fbeda778e3b938701b7138
strongym889-ship-it <strongym889@gmail.com>
2026-08-20 21:54:02 -0500

    Prueba - Error de cambio y autorización de usuario inadecuado en GitHub

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('✅ MongoDB conectado correctamente');
+    console.log('✅ MongoDB conectado correctamente a la DataBase');
   } catch (error) {
     console.error('❌ Error al conectar MongoDB:', error.message);
     process.exit(1);
